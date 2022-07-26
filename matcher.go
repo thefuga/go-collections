9f5a36322f3c4e1343f13cd6a2e519b4dd958cf1
Erick Fuga <erickcfuga@gmail.com>
2022-07-26 15:54:53 -0300

    split collection packages

--- matcher.go
+++ matcher.go
@@ -1,16 +1,8 @@
-package collection
+package collections
 
 import "reflect"
 
-type (
-	Matcher func(key any, value any) bool
-
-	Relational interface {
-		uint8 | uint16 | uint32 | uint64 | uint |
-			int8 | int16 | int32 | int64 | int |
-			float32 | float64 | string
-	}
-)
+type Matcher func(key any, value any) bool
 
 func KeyEquals(key any) Matcher {
 	return func(collectionKey any, _ any) bool {
