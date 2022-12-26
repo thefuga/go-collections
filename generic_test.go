9839247f3fa57b1d2fdbc3ee6567eb4f04a324c2
João Fernandes <joaofnds@joaofnds.com>
2022-12-26 17:55:19 -0300

    rename test cases

--- generic_test.go
+++ generic_test.go
@@ -1205,22 +1205,22 @@ func TestZip(t *testing.T) {
 			[][]int{{1, 2}, {3, 4}, {5, 6}},
 		},
 		{
-			"shorter 'a'",
+			"first slice shorter",
 			[][]int{{1, 3}, {2, 4, 6}},
 			[][]int{{1, 2}, {3, 4}},
 		},
 		{
-			"shorter 'b'",
+			"second slice shorter",
 			[][]int{{1, 3, 5}, {2, 4}},
 			[][]int{{1, 2}, {3, 4}},
 		},
 		{
-			"empty 'a'",
+			"first slice empty",
 			[][]int{{}, {1, 2, 3}},
 			[][]int{},
 		},
 		{
-			"empty 'b'",
+			"second slice empty",
 			[][]int{{1, 2, 3}, {}},
 			[][]int{},
 		},
