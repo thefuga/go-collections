fe703b15c95b7d59719387da30c7fd2a9823b3a3
João Fernandes <joaofnds@joaofnds.com>
2022-12-29 11:42:41 -0300

    feat(generic): add `Reverse`

--- generic.go
+++ generic.go
@@ -454,3 +454,10 @@ func Chunk[V any](slice []V, size int) [][]V {
 
 	return result
 }
+
+func Reverse[V any](slice []V) []V {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+	return slice
+}
